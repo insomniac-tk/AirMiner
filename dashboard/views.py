922048d8c533870ee41416c177af97af7c9ba5fa
insomniac-tk <shirleyhouse20@gmail.com>
2019-03-25 14:00:39 +0530

    More updates!

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from .models import Reading
-
+from django.utils import timezone
 # Create your views here.
 
 
 def index(request):
 	reading = Reading.objects.latest('reading_time')
-	context = {'reading' : reading}
+	curr_time = timezone.now
+	context = {'reading' : reading,'time':curr_time}
 	return render(request,'dashboard/index.html',context)
